7cde396d173e19c8e157e13b8d4c1b68483c3df8
mosharif30 <sharifkhani.mr@gmail.com>
2022-10-30 11:44:33 +0330

    pagination last 100

--- services/index.js
+++ services/index.js
@@ -5,7 +5,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 export const getPosts = async () => {
   const query = gql`
     query MyQuery {
-      postsConnection {
+      postsConnection(last: 100) {
         edges {
           cursor
           node {
