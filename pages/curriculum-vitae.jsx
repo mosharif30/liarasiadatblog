b1d9d4cff120da9e9fb3414413e2699cd72bc5e2
mosharif30 <sharifkhani.mr@gmail.com>
2022-11-22 18:21:32 +0330

    cv picture bug fix

--- pages/curriculum-vitae.jsx
+++ pages/curriculum-vitae.jsx
@@ -28,7 +28,7 @@ const About = ({ posts }) => {
               CURRICULUM VITAE
             </div>
             <img
-              src="./siadat2.jpg"
+              src="./siadat2.JPG"
               alt="Amir Siadat"
               className="object-top  h-96 w-96 object-cover shadow-lg "
             ></img>
